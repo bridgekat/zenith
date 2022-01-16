2099c6ed192027a5dfb13194af0f589ed3ed48f3
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2022-01-16 09:30:52 +0000

    Even more random stuff!

--- lcfstyle/LambdaPi1.hs
+++ lcfstyle/LambdaPi1.hs
@@ -1,5 +1,5 @@
--- Simple type theory / higher-order logic for experimentation
--- This variant of HOL is largely based on Andres Löh, Conor McBride & Wouter Swierstra's
+-- Dependent type theory for experimentation
+-- This variant of DTT is largely based on Andres Löh, Conor McBride & Wouter Swierstra's
 -- *A tutorial implementation of a dependently typed lambda calculus*...
 
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
@@ -7,40 +7,14 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleInstances #-}
 
-module HOL1 where
+module LambdaPi1 where
 
 import Data.List
 import Data.Char
 import Control.Monad
 
-data Kind =
-    Star
-  deriving (Eq)
 
-data Type =
-    TVar String
-  | TFun Type Type
-  deriving (Eq)
-
-showK :: Kind -> String
-showK Star = "*"
-
-showT :: Type -> String
-showT (TVar id) = id
-showT (TFun t1 t2) = "(" ++ showT t1 ++ " → " ++ showT t2 ++ ")"
-
-instance Show Kind where
-  show = showK
-
-instance Show Type where
-  show = showT
-
-data Info =
-    HasKind Kind
-  | HasType Type
-  deriving (Eq)
-
-type Context = [(String, Info)]
+type Context = [(String, Value)]
 
 -- Bound variables are represented using de Brujin indices
 -- (0 = binds to the deepest binder, 1 = escapes one binder, and so on)
@@ -52,8 +26,10 @@ data IC = Inferable | Checkable
 
 data Expr a where
   -- "Inferable": type can be determined by looking only at the subtree (bottom-up)
-  Ann :: Expr 'Checkable -> Type            -> Expr 'Inferable
+  Ann :: Expr 'Checkable -> Expr 'Checkable -> Expr 'Inferable
   Var :: VarName                            -> Expr 'Inferable
+  Star ::                                      Expr 'Inferable
+  Pi  :: Expr 'Checkable -> Expr 'Checkable -> Expr 'Inferable
   App :: Expr 'Inferable -> Expr 'Checkable -> Expr 'Inferable
   -- "Checkable": given an expected type, we can determine whether it is possible to
   -- fill in the "blanks" and make the subtree this type (top-down)
@@ -64,6 +40,8 @@ data Expr a where
 instance Eq (Expr a) where
   (==) (Ann x1 x2) (Ann y1 y2) = x1 == y1 && x2 == y2
   (==) (Var x1)    (Var y1)    = x1 == y1
+  (==) (Star)      (Star)      = True
+  (==) (Pi x1 x2)  (Pi y1 y2)  = x1 == y1 && x2 == y2
   (==) (App x1 x2) (App y1 y2) = x1 == y1 && x2 == y2
   (==) (Inf x1)    (Inf y1)    = x1 == y1
   (==) (Lam x1)    (Lam y1)    = x1 == y1
@@ -84,14 +62,16 @@ showName stk (Bound i) = stk !! i
 
 showE :: [String] -> [String] -> Expr a -> String
 showE used stk e = case e of
-  (Ann e1 t1)  -> "(" ++ showE used stk e1 ++ " : " ++ showT t1 ++ ")"
+  (Ann e1 e2)  -> "(" ++ showE used stk e1 ++ " : " ++ showE used stk e2 ++ ")"
   (Var x)      -> showName stk x
+  (Star)       -> "*"
+  (Pi e1 e2)   -> "(Π " ++ x' ++ " : " ++ showE used stk e1 ++ ", " ++ showE (x' : used) (x' : stk) e2 ++ ")" where x' = newName "a" used
   (App e1 e2)  -> "(" ++ showE used stk e1 ++ " " ++ showE used stk e2 ++ ")"
   (Inf e1)     -> showE used stk e1
   (Lam e1)     -> "(λ " ++ x' ++ ", " ++ showE (x' : used) (x' : stk) e1 ++ ")" where x' = newName "a" used
 
 inContextShowE :: Context -> Expr a -> String
-inContextShowE ls = showE (map fst . filter (\(_, i) -> case i of (HasType _) -> True; _ -> False) $ ls) []
+inContextShowE ls = showE (map fst ls) []
 
 instance Show (Expr a) where
   show = showE [] []
@@ -101,6 +81,8 @@ updateVars :: Int -> (Int -> VarName -> Expr 'Inferable) -> Expr a -> Expr a
 updateVars n f e = case e of
   (Ann e1 t1)  -> Ann (updateVars n f e1) t1
   (Var x)      -> f n x
+  (Star)       -> Star
+  (Pi e1 e2)   -> Pi (updateVars n f e1) (updateVars (n + 1) f e2)
   (App e1 e2)  -> App (updateVars n f e1) (updateVars n f e2)
   (Inf e1)     -> Inf (updateVars n f e1)
   (Lam e1)     -> Lam (updateVars (n + 1) f e1)
@@ -114,53 +96,51 @@ makeReplace t = updateVars 0 (\n x -> if x == Bound n then t else Var x)
 throwError :: String -> Either String a
 throwError = Left
 
--- Check if a type can have a given kind
-checkKind :: Context -> Type -> Kind -> Either String ()
-checkKind ctx (TVar x) Star =
-  case lookup x ctx of
-    (Just (HasKind Star)) -> Right ()
-    (Just (HasType t))    -> throwError ("Expected type, term " ++ x ++ " has type " ++ show t)
-    Nothing               -> throwError ("Unknown identifier: " ++ x)
-checkKind ctx (TFun t1 t2) Star =
-  do  checkKind ctx t1 Star;
-      checkKind ctx t2 Star;
-
--- Infer types by looking at the subtree
-inferType :: [Type] -> Context -> Expr 'Inferable -> Either String Type
-inferType stk ctx (Ann e t) =
-  do  checkKind ctx t Star;
-      checkType stk ctx e t;
-      return t;
+-- Infer types by looking at the subtree (storing inferred types in their normal form)
+inferType :: [Value] -> Context -> Expr 'Inferable -> Either String Value
+inferType stk ctx (Ann e1 e2) =
+  do  checkType stk ctx e2 VStar;
+      checkType stk ctx e1 e2';
+      return e2';
+  where e2' = eval [] e2
 inferType stk ctx (Var (Free x)) =
   case lookup x ctx of
-    (Just (HasType t)) -> return t
-    (Just (HasKind t)) -> throwError ("Expected term, type " ++ x ++ " has kind " ++ show t)
-    Nothing            -> throwError ("Unknown identifier: " ++ x)
+    (Just t) -> return t
+    Nothing  -> throwError ("Unknown identifier: " ++ x)
 inferType stk ctx (Var (Bound i)) =
   return (stk !! i)
+inferType stk ctx (Star) =
+  return VStar -- (n + 1)
+inferType stk ctx (Pi e1 e2) =
+  do  checkType stk ctx e1 VStar;
+      checkType (e1' : stk) ctx e2 VStar;
+      return VStar;
+  where e1' = eval [] e1;
 inferType stk ctx (App e1 e2) =
   do  t <- inferType stk ctx e1;
       case t of
-        (TFun t1 t2) -> do checkType stk ctx e2 t1; return t2;
-        _            -> throwError ("Expected function, given term has type " ++ show t)
+        (VPi t1 t2) -> do checkType stk ctx e2 t1; return (t2 (eval [] e2))
+        _           -> throwError ("Expected function, given term has type " ++ show (quote 0 t))
 
--- Check if subtree can have a given type
-checkType :: [Type] -> Context -> Expr 'Checkable -> Type -> Either String ()
+-- Check if subtree can have a given type (storing expected type in its normal form)
+checkType :: [Value] -> Context -> Expr 'Checkable -> Value -> Either String ()
 checkType stk ctx (Inf e) t =
   do  t' <- inferType stk ctx e;
-      unless (t == t') $ throwError ("Type mismatch, expected " ++ show t ++ ", inferred " ++ show t')
-checkType stk ctx (Lam e) (TFun t1 t2) =
-  checkType (t1 : stk) ctx e t2
+      unless (quote 0 t == quote 0 t') $ throwError ("Type mismatch, expected " ++ show (quote 0 t) ++ ", inferred " ++ show (quote 0 t'))
+checkType stk ctx (Lam e) (VPi t1' t2') =
+  do  checkType (undefined : stk) ((x', t1') : ctx) (makeReplace (Var (Free x')) e) (t2' (VNeutral (NFree x')))
+  where x' = "$" ++ show (length stk) -- Make a temporary variable
 checkType stk ctx (Lam e) t =
-  throwError ("Expected term of type " ++ show t ++ ", given term has function type")
+  throwError ("Expected term of type " ++ show (quote 0 t) ++ ", given term has function type")
 
--- Evaluation result
+-- Evaluation result (normal form)
 -- "Higher-order abstract syntax": use metalanguage constructs (Haskell functions) for higher-order values (functions)
 data Value =
-    VLam (Value -> Value)
+    VPi Value (Value -> Value)
+  | VLam (Value -> Value)
+  | VStar
   | VNeutral Neutral
 
--- Fully-reduced non-function values
 data Neutral =
     NFree String
   | NApp Neutral Value
@@ -169,10 +149,13 @@ eval :: [Value] -> Expr a -> Value
 eval stk (Ann e _) = eval stk e
 eval stk (Var (Free id)) = VNeutral (NFree id)
 eval stk (Var (Bound i)) = stk !! i
+eval stk (Star) = VStar
+eval stk (Pi e1 e2) = VPi (eval stk e1) (\x -> eval (x : stk) e2)
 eval stk (App e1 e2) =
   case eval stk e1 of
     (VLam f)     -> f v -- Let Haskell do the beta-reduction... (有必要吗？)
     (VNeutral f) -> VNeutral (NApp f v)
+    _            -> error "Illegal function application during evaluation"
   where
     v = eval stk e2
 eval stk (Inf i) = eval stk i
@@ -180,7 +163,9 @@ eval stk (Lam e) = VLam (\x -> eval (x : stk) e)
 
 -- Convert Haskell functions back to expressions in the subject language
 quote :: Int -> Value -> Expr 'Checkable
+quote i (VPi v f)    = Inf (Pi (quote i v) (quote (i + 1) (f (VNeutral (NFree ('$' : show i))))))
 quote i (VLam f)     = Lam (quote (i + 1) (f (VNeutral (NFree ('$' : show i))))) -- Substitute in a temporary variable
+quote i (VStar)      = Inf (Star)
 quote i (VNeutral f) = Inf (quoteN i f)
 
 quoteN :: Int -> Neutral -> Expr 'Inferable
@@ -191,21 +176,12 @@ quoteN i (NApp n v)        = App (quoteN i n) (quote i v)
 
 -- TEMP CODE
 
-id' = Lam (Inf (Var . Bound $ 0))
-const' = Lam (Lam (Inf (Var . Bound $ 1)))
+id' = Ann (Lam (Lam (Inf . Var . Bound $ 0))) (Inf $ Pi (Inf Star) (Inf $ Pi (Inf . Var . Bound $ 0) (Inf . Var . Bound $ 1)))
+ctx = [("False", eval [] (Var . Free $ "Bool")), ("Bool", eval [] Star)]
 
-tfree = TVar
-free x = Inf (Var . Free $ x)
-
-term1 = Ann id' (tfree "a" `TFun` tfree "a") `App` free "y"
-term2 = Ann const' ((tfree "b" `TFun` tfree "b") `TFun` (tfree "a" `TFun` (tfree "b" `TFun` tfree "b"))) `App` id' `App` free "y"
-
-env1 = [("y", HasType (tfree "a")), ("a", HasKind Star)]
-env2 = ("b", HasKind Star) : env1
-
-ex1 = quote 0 (eval [] term1)
-ex2 = quote 0 (eval [] term2)
-ex3 = inferType [] env1 term1
-ex4 = inferType [] env2 term2
+Right ex1' = inferType [] ctx (App id' (Inf . Var . Free $ "Bool"))
+ex1 = quote 0 ex1'
+Right ex2' = inferType [] ctx (App (App id' (Inf . Var . Free $ "Bool")) (Inf . Var . Free $ "False"))
+ex2 = quote 0 ex2'
 
 
---
+++ lcfstyle/README.md
@@ -0,0 +1,3 @@
+## Experiments and LCF-style verifiers
+
+Most things in this folder are irrelevant to the project...?
