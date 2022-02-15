f5af61f72507cfd9ca92f1b497ee32373f2266f3
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2022-02-15 13:57:04 +0000

    Test json-rpc-cxx
    LTO fails mysteriously?

---
+++ src/testclient.cpp
@@ -0,0 +1,29 @@
+#include <jsonrpccxx/common.hpp>
+#include <jsonrpccxx/client.hpp>
+#include <cpp-httplib/httplib.h>
+
+
+// Adapted from: https://github.com/jsonrpcx/json-rpc-cxx/tree/master/examples
+class HttpClientConnector: public jsonrpccxx::IClientConnector {
+public:
+  explicit HttpClientConnector(const std::string &host, int port):
+           httpClient(host.c_str(), port) {}
+
+  // Sends request and returns response
+  std::string Send(const std::string &request) override {
+    auto res = httpClient.Post("/jsonrpc", request, "application/json");
+    if (!res || res->status != 200) {
+      throw jsonrpccxx::JsonRpcException(-32003, "client connector error, received status != 200");
+    }
+    return res->body;
+  }
+
+private:
+  httplib::Client httpClient;
+};
+
+
+int main() {
+
+  return 0;
+}
---
+++ src/testserver.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <iomanip>
+#include <jsonrpccxx/common.hpp>
+#include <jsonrpccxx/server.hpp>
+#include <cpp-httplib/httplib.h>
+
+
+// Adapted from: https://github.com/jsonrpcx/json-rpc-cxx/tree/master/examples
+class HttpServerConnector {
+public:
+  explicit HttpServerConnector(jsonrpccxx::JsonRpcServer& rpcServer, const std::string& host, int port):
+           thread(), httpServer(), rpcServer(rpcServer), host(host), port(port) {
+    // Handles POST requests to `/jsonrpc`
+    httpServer.Post("/jsonrpc", [this] (const httplib::Request& req, httplib::Response& res) {
+      postAction(req, res);
+    });
+  }
+  virtual ~HttpServerConnector() { stopListening(); }
+
+  bool startListening() {
+    if (httpServer.is_running()) return false; // Already started
+    thread = std::thread([this] () { httpServer.listen(host.c_str(), port); });
+    return true;
+  }
+
+  void stopListening() {
+    if (!httpServer.is_running()) return;
+    httpServer.stop();
+    thread.join();
+  }
+
+private:
+  std::thread thread;
+  httplib::Server httpServer;
+  jsonrpccxx::JsonRpcServer& rpcServer;
+  
+  std::string host;
+  int port;
+
+  void postAction(const httplib::Request& req, httplib::Response& res) {
+    // Writes response using `rpcServer`
+    res.status = 200;
+    res.set_content(this->rpcServer.HandleRequest(req.body), "application/json");
+  }
+};
+
+
+using std::cin, std::cout, std::endl;
+
+int main() {
+
+  jsonrpccxx::JsonRpc2Server rpcServer;
+  HttpServerConnector httpServer(rpcServer, "localhost", 8484);
+  cout << "Starting HTTP server: " << std::boolalpha << httpServer.startListening() << endl;
+  cout << "Enter 0 to exit" << endl;
+
+  while (true) {
+    int in;
+    cin >> in;
+    if (in == 0) break;
+  }
+
+  return 0;
+}
