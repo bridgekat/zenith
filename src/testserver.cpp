971579317baf29e3840ff63bf992a907c63e9e70
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2022-02-16 10:34:15 +0000

    Finally made this working
    Note: dependencies modified locally

--- src/testserver.cpp
+++ src/testserver.cpp
@@ -1,66 +1,137 @@
 #include <iostream>
 #include <iomanip>
+#include <utility>
 #include <string>
+#include <sstream>
+#include <fstream>
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
 #include <jsonrpccxx/server.hpp>
-#include <cpp-httplib/httplib.h>
+#include "core/base.hpp"
+
+using std::cin, std::cout, /* std::cerr, */ std::endl;
+using std::pair, std::string, nlohmann::json;
+using jsonrpccxx::MethodHandle, jsonrpccxx::NotificationHandle;
+
+std::ofstream cerr("D:/log.txt");
 
 
-// Adapted from: https://github.com/jsonrpcx/json-rpc-cxx/tree/master/examples
-class HttpServerConnector {
-public:
-  explicit HttpServerConnector(jsonrpccxx::JsonRpcServer& rpcServer, const std::string& host, int port):
-           thread(), httpServer(), rpcServer(rpcServer), host(host), port(port) {
-    // Handles POST requests to `/jsonrpc`
-    httpServer.Post("/jsonrpc", [this] (const httplib::Request& req, httplib::Response& res) {
-      postAction(req, res);
-    });
+json initialize(const json&) {
+  json res = {
+    { "capabilities", {
+      // TODO: add server capabilities here
+      { "hoverProvider", false },
+    } },
+    { "serverInfo", {
+      { "name", "ApiMu Test Server" },
+      { "version", "0.0.1" },
+    } },
+  };
+  return res;
+}
+
+void initialized(const json&) {}
+
+json test(const json& j) {
+  json res = { { "echo", j.value("str", "") } };
+  return res;
+}
+
+json shutdown(const json&) {
+  json res = {};
+  return res;
+}
+
+void exit_(const json&) {
+  std::exit(0);
+}
+
+
+string getline() {
+  string res = "";
+///*
+  std::getline(cin, res);
+  if (!res.empty() && res.back() == '\r') res.pop_back();
+  cerr << "Got [" << res << "] " << std::boolalpha << cin.eof() << endl;
+  return res;
+//*/
+/*
+  char c = static_cast<char>(cin.get());
+  while (c != std::ios::traits_type::eof() && c != '\r' && c != '\n') {
+    res += c;
+    c = static_cast<char>(cin.get());
   }
-  virtual ~HttpServerConnector() { stopListening(); }
+  if (c == '\r') cin.ignore(); // Ignore "\n" after "\r"
+  cerr << "Got [" << res << "] " << std::boolalpha << cin.eof() << endl;
+  return res;
+*/
+}
 
-  bool startListening() {
-    if (httpServer.is_running()) return false; // Already started
-    thread = std::thread([this] () { httpServer.listen(host.c_str(), port); });
-    return true;
-  }
-
-  void stopListening() {
-    if (!httpServer.is_running()) return;
-    httpServer.stop();
-    thread.join();
-  }
-
-private:
-  std::thread thread;
-  httplib::Server httpServer;
-  jsonrpccxx::JsonRpcServer& rpcServer;
-
-  std::string host;
-  int port;
-
-  void postAction(const httplib::Request& req, httplib::Response& res) {
-    // Writes response using `rpcServer`
-    res.status = 200;
-    res.set_content(this->rpcServer.HandleRequest(req.body), "application/json");
-  }
-};
-
-
-using std::cin, std::cout, std::endl;
-using std::string;
+pair<string, string> split(const string& s) {
+  size_t p = s.find(": ");
+  if (p == string::npos) throw std::logic_error("No \": \" found in [" + s + "]"); // ???
+  return { s.substr(0, p), s.substr(p + 2) };
+}
 
 int main() {
   jsonrpccxx::JsonRpc2Server rpcServer;
-  std::function<string(const string&)> func = [] (const string& s) { return s; };
-  rpcServer.Add("test", jsonrpccxx::GetHandle(func), { "param_name" });
 
-  HttpServerConnector httpServer(rpcServer, "localhost", 8484);
-  cout << "Starting HTTP server: " << std::boolalpha << httpServer.startListening() << endl;
-  cout << "Enter 0 to exit" << endl;
+  rpcServer.Add("initialize", static_cast<MethodHandle>(initialize));
+  rpcServer.Add("initialized", static_cast<NotificationHandle>(initialized));
+  rpcServer.Add("test", static_cast<MethodHandle>(test));
+  rpcServer.Add("shutdown", static_cast<MethodHandle>(shutdown));
+  rpcServer.Add("exit", static_cast<NotificationHandle>(exit_));
 
+  // ???
+#ifdef _WIN32
+	_setmode(_fileno(stdin), _O_BINARY);
+	_setmode(_fileno(stdout), _O_BINARY);
+#endif
+
+  // Main loop
   while (true) {
-    int in;
-    cin >> in;
-    if (in == 0) break;
+    string s;
+    // Read header part
+    // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#headerPart
+    size_t n = 0;
+    s = getline();
+    while (s != "") {
+      // We just disregard all header fields... except `Content-Length`!
+      auto [key, value] = split(s);
+      cerr << "[" << key << "] = [" << value << "]" << endl;
+      if (key == "Content-Length") {
+        std::stringstream ss(value);
+        ss >> n;
+      } else if (key == "Content-Type") {
+        if (value != "application/vscode-jsonrpc; charset=utf-8" &&
+            value != "application/vscode-jsonrpc; charset=utf8")
+          throw std::logic_error("Content-Type = " + value);
+      } else throw std::logic_error("Key = " + key);
+      // Get next line
+      s = getline();
+    }
+    // End of header, get content
+    s.resize(n);
+    cin.read(s.data(), n);
+    if (cin.eof()) std::exit(0); // ???
+    // Handle request
+    cerr << "Request = [" << s << "]" << endl;
+    s = rpcServer.HandleRequest(s);
+    if (!s.empty()) {
+      cout << "Content-Length: " << s.size() << "\r\n";
+      cout << "Content-Type: application/vscode-jsonrpc; charset=utf-8\r\n";
+      cout << "\r\n";
+      cout.write(s.data(), s.size());
+      cout.flush();
+      // Debug output
+      cerr << "Content-Length: " << s.size() << endl;
+      cerr << "Content-Type: application/vscode-jsonrpc; charset=utf-8" << endl;
+      cerr << endl;
+      cerr << s << endl << endl;
+    }
   }
+
   return 0;
 }
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -2,13 +2,30 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+
+// ===== TEMP CODE BEGIN =====
+
+// This method is called when the extension is activated
+// Refer to: https://github.com/microsoft/vscode-extension-samples/blob/main/lsp-sample/client/src/extension.ts
+// Refer to: https://github.com/digama0/mm0/blob/master/vscode-mm0/src/extension.ts
 export function activate(context: vscode.ExtensionContext) {
-  
+
+  // ===== Output something =====
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "apimu" is now active!');
+
+  // ===== Register commands =====
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -16,11 +33,71 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('apimu.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from ApiMu!');
+    vscode.window.showInformationMessage('喵喵喵🐱');
+    if (client) {
+      client.sendRequest('test', { 'str': 'gg' }).then((val: unknown) => {
+        if (typeof val === 'object' && val) {
+          let key = 'echo' as keyof typeof val;
+          vscode.window.showInformationMessage('Got message: ' + val[key]);
+        }
+      }, () => {});
+    }
   });
 
-  context.subscriptions.push(disposable);
+  let restartServer = vscode.commands.registerCommand('apimu.restartServer', () => {
+    if (client) { client.stop().then(startServer, startServer); }
+    else { startServer(); }
+  });
+
+  let shutdownServer = vscode.commands.registerCommand('apimu.shutdownServer', () => {
+    if (client) {
+      client.stop().then(
+        () => { vscode.window.showInformationMessage('Server stopped'); },
+        () => { vscode.window.showInformationMessage('Server stopped'); }
+      );
+    }
+  });
+
+  context.subscriptions.push(disposable, restartServer, shutdownServer);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function startServer() {
+  // Server executable path
+  let config = vscode.workspace.getConfiguration('apimu');
+  let executablePath: string = config.get('executablePath') || "testserver";
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { command: executablePath, args: ['server'] },
+    debug: { command: executablePath, args: ['server', '--debug'] }
+  };
+
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for .mu documents
+    documentSelector: [{ scheme: 'file', language: 'apimu' }]
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'apimu-lsp-test-client',
+    'ApiMu Language Server Protocol Test Client',
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server.
+  client.start();
+
+  // Print status
+  vscode.window.showInformationMessage('Attempted to start server.');
+}
+
+// This method is called when the extension is deactivated
+export function deactivate() {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
